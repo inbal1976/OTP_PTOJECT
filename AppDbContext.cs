de218b364b7135e90658f58cde86907f8d31dfcc
inbal1976 <inbal.ka@abra-it.com>
2025-04-03 14:14:07 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OTPProject;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Otp> Otp { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
---
+++ Otp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OTPProject
+{
+    public class Otp
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public DateTime ExpirationTime { get; set; }
+    }
+}
